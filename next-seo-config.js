270108d4d7129cb4c87dc93cdd5a2d5de78e63cd
Michel Lavezzo <michellavezzo@gmail.com>
2025-09-26 22:34:10 -0300

    SEO config

---
+++ next-seo-config.js
@@ -0,0 +1,13 @@
+export const NEXT_SEO_DEFAULT = {
+    title: "Michel Lavezzo - Computer Engineer",
+    description: "Computer Engineer | Web & Mobile Developer | Tech Enthusiast",
+    openGraph: {
+        type: "website",
+        locale: "en_IE",
+        // url: "https://www.url.ie/a",
+        title: "Michel Lavezzo - Computer Engineer",
+        description:
+            "Computer Engineer | Web & Mobile Developer | Tech Enthusiast",
+        siteName: "Michel Lavezzo - Computer Engineer",
+    },
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,32 @@
-import "../styles/globals.css";
-
-// components
-import Layout from "../components/Layout";
-import Transition from "../components/Transition";
-
-// router
-import { useRouter } from "next/router";
-
-// framer motion
-import { AnimatePresence, motion } from "framer-motion";
+import "../styles/globals.css"
+import Head from "next/head"
+import Layout from "../components/Layout"
+import Transition from "../components/Transition"
+import { NextSeo } from "next-seo"
+import { NEXT_SEO_DEFAULT } from "../next-seo-config"
+import { useRouter } from "next/router"
+import { AnimatePresence, motion } from "framer-motion"
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
-  );
+    const router = useRouter()
+    return (
+        <>
+            <NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />
+            <Head>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <Layout>
+                <AnimatePresence mode='wait'>
+                    <motion.div key={router.route} className='h-full'>
+                        <Transition />
+                        <Component {...pageProps} />
+                    </motion.div>
+                </AnimatePresence>
+            </Layout>
+        </>
+    )
 }
 
-export default MyApp;
+// todo: https://dev.to/trinity_/como-gerenciar-seo-em-nextjs-com-next-seo-1pb3
+
+export default MyApp
